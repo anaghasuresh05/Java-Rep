77daa70c52477f25ccf926bfe36937bcc5fb9861
Java <anaghasuresh2005@gmail.com>
2025-07-11 12:12:12 +0530

    initial commit1

--- fibonacci/src/Main.java
+++ fibonacci/src/Main.java
@@ -1,18 +1,29 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
 public class Main {
-    public int fib(int n) {
-        if (n == 0) {
-            return (0);
-        } else if (n == 1) {
-            return (1);
-        } else {
-            return (fib(n - 1) + fib(n - 2));
+    static long[] fibArray;
+    public long fib(int n) {
+        if (fibArray[n] == -1) {
+            fibArray[n] = fib(n - 1) + fib(n - 2);
         }
+        return (fibArray[n]);
     }
 
-        public static void main (String[]args){
+    public static void main (String[]args){
             Main x = new Main();
-            System.out.println(x.fib(10));
+            Scanner scan=new Scanner(System.in);
+            int N=scan.nextInt();
+            fibArray=new long[N+1];
+            for(int i=0;i<=N;i++)
+                fibArray[i]=-1;
+        fibArray[0]=0;
+        fibArray[1]=1;
+        System.out.printf("the number is %d\n",N);
+        System.out.printf("the nth term is %d\n",x.fib(N));
+        System.out.print(fibArray[0]);
+        for(int i=1;i<+N;i++)
+            System.out.printf(" %d ",fibArray[i]);
         }
 }
