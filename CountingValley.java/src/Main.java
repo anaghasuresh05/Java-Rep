1bd0a10d439fcb74abce07cf051cb44ec22fd302
Java <anaghasuresh2005@gmail.com>
2025-07-11 14:31:13 +0530

    initial commit2

---
+++ CountingValley.java/src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Enter the pattern:");
+        Scanner scan=new Scanner(System.in);
+        String movements=scan.nextLine();
+        short altitude=0;
+        short valleyCount=0;
+       for(int i=0;i<movements.length();i++)[
+               if(movements.charAt(i)=='U') {
+                   altitude += 1;
+                   else if (movements.charAt(i) == 'D') {
+                       altitude -= 1;
+                   }
+                   if (altitude == 0 && movements.charAt(i) == 'U') {
+                       valleyCount++;
+                   }
+               }
+               System.out.printf("Number of valleys; %d",valleyCount);
+    }
+}
+
